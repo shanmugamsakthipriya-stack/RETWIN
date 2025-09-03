31fcf851be12f09ef6a50973d4ffbc3f8f70cf8f
shanmugamsakthipriya-stack <shanmugam.sakthipriya@gmail.com>
2025-09-03 13:42:23 +0530

    Create config.py

---
+++ etl/config.py
@@ -0,0 +1,12 @@
+import os
+
+SECURITY_TOKEN = os.environ.get("SECURITY_TOKEN")
+AZURE_PG_HOST = os.environ.get("AZURE_PG_HOST")
+AZURE_PG_DB = os.environ.get("AZURE_PG_DB")
+AZURE_PG_USER = os.environ.get("AZURE_PG_USER")
+AZURE_PG_PASSWORD = os.environ.get("AZURE_PG_PASSWORD")
+SMTP_SERVER = os.environ.get("SMTP_SERVER")
+SMTP_PORT = int(os.environ.get("SMTP_PORT", 587))
+SMTP_USER = os.environ.get("SMTP_USER")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
+ALERT_EMAIL = os.environ.get("ALERT_EMAIL", SMTP_USER)
